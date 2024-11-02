025a7bad5b58b016d03d6251dafe94475c300fdf
Volodymyr1910 <priestvolodya@gmail.com>
2024-11-02 10:56:19 +0100

    02-11-2024 Update

--- AIT-TR course 2024/Main Folder/Codes/HomeWorks/November/HW1/planets/PlanetsApplication.java
+++ AIT-TR course 2024/Main Folder/Codes/HomeWorks/November/HW1/planets/PlanetsApplication.java
@@ -11,53 +11,39 @@ import java.util.*;
 //    //mass;
 //    //number of satellites.
 
-public class PlanetsApplication implements Comparable <Planets>{
+public class PlanetsApplication {
 
     public static void main(String[] args) {
-        List<Planets> planetsList = List.of(Planets.MERCURY, Planets.Venus, Planets.MARS,Planets.URANUS, Planets.EARTH, Planets.NEPTUNE, Planets.JUPITER, Planets.SATURN);
+        // List<Planets> planetsList = List.of(Planets.MERCURY, Planets.Venus, Planets.MARS,Planets.URANUS, Planets.EARTH, Planets.NEPTUNE, Planets.JUPITER, Planets.SATURN);
+        List<Planets> planetsList = new ArrayList<>();
+
+        planetsList.add(Planets.MERCURY);
+        planetsList.add(Planets.Venus);
+        planetsList.add(Planets.MARS);
+        planetsList.add(Planets.URANUS);
+        planetsList.add(Planets.EARTH);
+        planetsList.add(Planets.NEPTUNE);
+        planetsList.add(Planets.JUPITER);
+        planetsList.add(Planets.SATURN);
         ListMethods.printList(planetsList, "");
+        System.out.println();
 
-        // и дальше, как я ни старался, двинуться не выходит ---- UnsupportedOperationException
-        planetsList.sort(Comparator.comparing(Planets::getRangeToSun));
-        //planetsList.sort(Planets::compareTo);
-        //sortByAll(planetsList,"rangeToSun");
-        ListMethods.printList(planetsList, "after sort order of location in the solar system ");
+        Collections.sort(planetsList, Comparator.comparing(Planets::getRangeToSun));
+        ListMethods.printList(planetsList, "after sort by order of location in the solar system ");
+        System.out.println();
 
+        Collections.sort(planetsList, Comparator.comparing(Planets::getName));
+        ListMethods.printList(planetsList, "after sort by name ");
+        System.out.println();
 
+        Collections.sort(planetsList, Comparator.comparing(Planets::getMassRelativeEarth));
+        ListMethods.printList(planetsList, "after sort by mass relative Earth ");
+        System.out.println();
 
+        Collections.sort(planetsList, Comparator.comparing(Planets::getNumberOfSatellites));
+        ListMethods.printList(planetsList, "after sort by number of satellites ");
+        System.out.println();
 
-       // sortByAll(planetsList);
-        //Collections.sort(planetsList);
-        //ListMethods.printList(planetsList, " after sort ");
-        //List<Planets> planetsList = List.of();
     }//end main
 
-
-
-    public static List<Planets> sortByAll (List <Planets> list, String fieldForSort) {
-
-        Comparator<Planets> compByAll = new Comparator<Planets>() {
-            @Override
-            public int compare(Planets o1, Planets o2) {
-                int res = 0;
-                switch (fieldForSort){
-                    case "name" ->  res = o1.getName().compareTo(o2.getName());
-                    case "rangeToSun" ->   res = Long.compare(o1.getRangeToSun(),o2.getRangeToSun());
-                    case "massRelativeEarth" ->   res = Double.compare(o1.getMassRelativeEarth(), o2.getMassRelativeEarth());
-                    case "numberOfSatellites" ->  res =  Integer.compare(o1.getNumberOfSatellites(), o2.getNumberOfSatellites());
-                }
-                return res;
-            }
-        };
-
-        list.sort(compByAll);
-        //Collections.sort(Comparator.comparing(Planets::getRangeToSun));
-        return list;
-    }//end sortByAll
-
-
-    @Override
-    public int compareTo(Planets o) {
-        return this.compareTo(o);
-    }
 }//end class
